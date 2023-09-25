1670bfd9b0354d6fa1903cbfd441c21be0e74210
GCHQ-Developer-741 <117368893+GCHQ-Developer-741@users.noreply.github.com>
2023-09-25 12:56:27 +0100

    feat(react): added react stories for icon support
    added react stories and docs for how to add icons to tables

--- packages/web-components/src/components.d.ts
+++ packages/web-components/src/components.d.ts
@@ -1321,7 +1321,7 @@ export namespace Components {
         /**
           * Sets the styling for the items in the pagination bar.
          */
-        "appearance"?: IcThemeForeground;
+        "appearance"?: IcThemeForeground1;
         /**
           * The label which will be used in place of 'items' if paginationType is data. Should be capitalised.
          */
@@ -4097,7 +4097,7 @@ declare namespace LocalJSX {
         /**
           * Sets the styling for the items in the pagination bar.
          */
-        "appearance"?: IcThemeForeground;
+        "appearance"?: IcThemeForeground1;
         /**
           * The label which will be used in place of 'items' if paginationType is data. Should be capitalised.
          */
