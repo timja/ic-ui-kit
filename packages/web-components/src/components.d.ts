20a350036c32841155caeab0bb80fef25458c3ca
GCHQ-Developer-741 <117368893+GCHQ-Developer-741@users.noreply.github.com>
2023-09-29 14:44:49 +0100

    feat(react): added react stories for ic-title-bar
    added react examples and docs for ic-title-bar, along with usage with data table

--- packages/web-components/src/components.d.ts
+++ packages/web-components/src/components.d.ts
@@ -1336,7 +1336,7 @@ export namespace Components {
         /**
           * Sets the styling for the items in the pagination bar.
          */
-        "appearance"?: IcThemeForeground;
+        "appearance"?: IcThemeForeground1;
         /**
           * The label which will be used in place of 'items' if paginationType is data. Should be capitalised.
          */
@@ -2165,10 +2165,6 @@ export namespace Components {
           * The description that is displayed below the `header` and `metadata`. Can be overridden with the `description` slot.
          */
         "description"?: string;
-        /**
-          * If `true`, will apply a background colour and a bottom border to the title bar.
-         */
-        "fullWidth"?: boolean;
         /**
           * The header of the title bar. Can be overridden with the `header` slot. If used with an ic-data-table it will default to the table's `caption` unless overridden.
          */
@@ -4164,7 +4160,7 @@ declare namespace LocalJSX {
         /**
           * Sets the styling for the items in the pagination bar.
          */
-        "appearance"?: IcThemeForeground;
+        "appearance"?: IcThemeForeground1;
         /**
           * The label which will be used in place of 'items' if paginationType is data. Should be capitalised.
          */
@@ -5114,10 +5110,6 @@ declare namespace LocalJSX {
           * The description that is displayed below the `header` and `metadata`. Can be overridden with the `description` slot.
          */
         "description"?: string;
-        /**
-          * If `true`, will apply a background colour and a bottom border to the title bar.
-         */
-        "fullWidth"?: boolean;
         /**
           * The header of the title bar. Can be overridden with the `header` slot. If used with an ic-data-table it will default to the table's `caption` unless overridden.
          */
