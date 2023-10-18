a3f02f26ad41b74867e83c51db6ce31c9f618ecb
ad9242 <116102637+ad9242@users.noreply.github.com>
2023-10-18 13:05:31 +0000

    fix(web-components): remove unused icclear event and fix icchange
    removes unused icclear event and fixes issue with icchange emit same value multiple times

--- packages/web-components/src/components.d.ts
+++ packages/web-components/src/components.d.ts
@@ -3330,10 +3330,6 @@ declare namespace LocalJSX {
          */
         "onIcChange"?: (event: IcDateInputCustomEvent<{ value: Date }>) => void;
         /**
-          * Emitted when value is cleared with clear button
-         */
-        "onIcClear"?: (event: IcDateInputCustomEvent<void>) => void;
-        /**
           * Emitted when the input gains focus.
          */
         "onIcFocus"?: (event: IcDateInputCustomEvent<{ value: Date }>) => void;
